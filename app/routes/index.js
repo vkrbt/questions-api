fde8e5032dba81c71e38d5500e81f7987bff6995
Vladsislav Korbut <v.korbut8@gmail.com>
2018-03-06 20:47:50 +0300

    Some code refactoting

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,8 @@ const user = require('./user');
 const router = new Router();
 
 router.get('/users', user.getAll);
-router.post('/register', user.register);
+router.post('/register', user.postRegister);
+router.post('/login', user.postLogin);
 
 router.get('/questions', question.getAll);
 router.post('/questions', question.create);
--- app/routes/user.js
+++ app/routes/user.js
@@ -5,23 +5,62 @@ const getAll = async (ctx) => {
   ctx.body = users;
 };
 
-const register = async (ctx) => {
-  const { login, password } = ctx.request.body;
+const postRegister = async (ctx) => {
   try {
+    const { login, password } = ctx.request.body;
     const user = await User.create({
       login,
       password,
     });
-    ctx.body = {
-      message: `User ${user.login} has been created.`,
-    };
+    if (user) {
+      ctx.body = {
+        message: `User ${user.login} has been created.`,
+      };
+    } else {
+      throw user;
+    }
   } catch (err) {
+    if (err.errors) {
+      ctx.status = 400;
+      ctx.body = {
+        message: err.errors && err.errors[0].message,
+      };
+    } else {
+      ctx.status = 500;
+      ctx.body = {
+        message: 'An error has been occured',
+      };
+    }
+  }
+};
+
+const postLogin = async (ctx) => {
+  try {
+    const { login, password } = ctx.request.body;
+    const user = await User.findOne({
+      where: {
+        login,
+        password,
+      },
+    });
+    if (user) {
+      ctx.body = {
+        message: 'Success!',
+      };
+    } else {
+      throw user;
+    }
+  } catch (err) {
+    console.log(err);
     ctx.status = 400;
-    console.log(err.e);
+    ctx.body = {
+      message: 'No such user or password is wrong',
+    };
   }
 };
 
 module.exports = {
   getAll,
-  register,
+  postRegister,
+  postLogin,
 };
